1076c0dbb660576bb2d88488dbafe1ba967a0a3d
Nilogrib <142620260+Nilogrib@users.noreply.github.com>
2023-08-19 19:28:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const https = require("https");
+const bodyParser= require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");  
+});
+app.post("/",function(req,res){
+    const query = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units=metric&appid=e972f5cecfe3389b2f86c78bf41c2ad0"
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
+            res.write("<p>The weather is currently " + weatherDescription+"<p>");
+            res.write("<h1>The temperature in "+query+" is " + temp + " degree Celsius.</h1>");
+            res.write("<img src="+imageURL+">");
+            res.send()
+        })
+    })
+})
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000."); 
+})
